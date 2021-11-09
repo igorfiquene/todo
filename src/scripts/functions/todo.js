5e504085d2cd61d5dd636106a822be8540c8a729
igorfiquene <igorfpessoa@gmail.com>
2021-11-09 11:04:24 -0300

    dev: to pull marcelo

--- src/scripts/functions/todo.js
+++ src/scripts/functions/todo.js
@@ -12,22 +12,24 @@ export default function () {
 	});
 
 	function setNewTask(newTask) {
-		const newTaskItem = document.createElement('li');
+		createLi('list-item', newTask);
+		setLocalStorage(newTask);
+	}
 
-		newTaskItem.setAttribute('class', 'list-item');
+	const createLi = (className, newTask) => {
+		const newTaskItem = document.createElement('li');
+		newTaskItem.setAttribute('class', className);
 		newTaskItem.innerHTML = newTask;
 		listTasks.appendChild(newTaskItem);
+	};
 
-		setLocalStorage(newTask);
-
-
-	}
+	
 
 	function setLocalStorage(inputValue) {
 		var task = [];
-    task = JSON.parse(localStorage.getItem('item')) || [];
-    task.push(inputValue);
-    localStorage.setItem('item', JSON.stringify(task));
+		task = JSON.parse(localStorage.getItem('item')) || [];
+		task.push(inputValue);
+		localStorage.setItem('item', JSON.stringify(task));
 	}
 	
 }
